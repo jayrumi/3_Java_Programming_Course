4dc2616f172e77036a309a766091461531600ad8
Eugeny_Rumyantsev <jrumyantsev@gmail.com>
2017-06-16 19:19:12 +0300

    Complited a method triangleSquare

--- HW1_Tetrahedron/src/by/erumyantsev/javacourse3/action/TetrahedronAction.java
+++ HW1_Tetrahedron/src/by/erumyantsev/javacourse3/action/TetrahedronAction.java
@@ -14,7 +14,16 @@ public class TetrahedronAction {
         Point edgeBC = coordinateEdge.edge(pointB, pointC);
         Point edgeAC = coordinateEdge.edge(pointA, pointC);
 
-gi
+        float lengthEdgeAB = (float) Math.sqrt(Math.pow(edgeAB.getX(),2)+Math.pow(edgeAB.getY(),2)+Math.pow(edgeAB.getZ(),2));
+        float lengthEdgeBC = (float) Math.sqrt(Math.pow(edgeBC.getX(),2)+Math.pow(edgeBC.getY(),2)+Math.pow(edgeBC.getZ(),2));
+        float lengthEdgeAC = (float) Math.sqrt(Math.pow(edgeAC.getX(),2)+Math.pow(edgeAC.getY(),2)+Math.pow(edgeAC.getZ(),2));
 
+        float perimetrABC = (lengthEdgeAB+lengthEdgeBC+lengthEdgeAC)/2;
+
+        float squareABC = (float) Math.sqrt(perimetrABC*(perimetrABC-lengthEdgeAB)*(perimetrABC-lengthEdgeBC)*(perimetrABC-lengthEdgeAC));
+
+        return squareABC;
     }
+
+    public float squareSurface()
 }
