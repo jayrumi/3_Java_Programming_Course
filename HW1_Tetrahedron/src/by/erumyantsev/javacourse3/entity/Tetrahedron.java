a3440d1e80d2d9f196d5b95794e494844887b90c
Eugeny_Rumyantsev <jrumyantsev@gmail.com>
2017-06-16 19:13:01 +0300

    Complited a method triangleSquare

--- HW1_Tetrahedron/src/by/erumyantsev/javacourse3/entity/Tetrahedron.java
+++ HW1_Tetrahedron/src/by/erumyantsev/javacourse3/entity/Tetrahedron.java
@@ -1,9 +1,16 @@
 package by.erumyantsev.javacourse3.entity;
 
+import java.util.List;
+import java.util.Observer;
+
 /**
  * Created by Yauheni_Rumiantsau on 6/15/2017.
  */
 public class Tetrahedron {
+
+    private static final Logger LOGGER = logManager.getLogger();
+
+    //private List<Observer>
 
     private Point pointA;
     private Point pointB;
