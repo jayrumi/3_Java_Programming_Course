c183cefa8a4c7a75809ab07c96bfbee006b8c2ac
Eugeny_Rumyantsev <jrumyantsev@gmail.com>
2017-07-28 21:17:29 +0300

    Completed ToyFactory

--- HW2_GameRoom/src/org/rumyantsev/gameroom/toy/ToyFactory.java
+++ HW2_GameRoom/src/org/rumyantsev/gameroom/toy/ToyFactory.java
@@ -1,20 +1,33 @@
 package org.rumyantsev.gameroom.toy;
 
-import org.rumyantsev.gameroom.type.ToySize;
-import org.rumyantsev.gameroom.type.ToyType;
+import org.rumyantsev.gameroom.exception.ToyException;
+import org.rumyantsev.gameroom.type.*;
 
 /**
  * Created by jayrumi on 7/28/2017.
  */
 public class ToyFactory {
 
-    public Toy createToy(String[] toyDescription){
+    public Toy createToy(String[] toyDescription) throws ToyException {
 
         ToyType toyType = ToyType.valueOf(toyDescription[0]);
-
-        switch (toyType.name().toUpperCase()){
-            case "BALL":
-                return new Ball((ToySize).valueOf(toyDescription[1]), toyDescription[2], Float.parseFloat(toyDescription[3]), toyDescription[4], toyDescription[5], toyDescription[6]);
+        ToySize toySize = ToySize.valueOf(toyDescription[1]);
+        ToyMatherial toyMatherial = ToyMatherial.valueOf(toyDescription[2]);
+        AgeGroup ageGroup = AgeGroup.valueOf(toyDescription[3]);
+        try {
+            switch (toyType.name().toUpperCase()) {
+                case "BALL":
+                    BallType ballType = BallType.valueOf(toyDescription[6]);
+                    return new Ball(toySize, toyMatherial, Float.parseFloat(toyDescription[4]), ageGroup, Float.parseFloat(toyDescription[5]), ballType);
+                case "CAR":
+                    return new Car(toySize, toyMatherial, Float.parseFloat(toyDescription[4]), ageGroup, Integer.parseInt(toyDescription[5]));
+                case "CUBE":
+                    return new Cube(toySize, toyMatherial, Float.parseFloat(toyDescription[4]), ageGroup, Integer.parseInt(toyDescription[5]), toyDescription[6], Boolean.parseBoolean(toyDescription[7]));
+                case "DOLL":
+                    return new Doll(toySize, toyMatherial, Float.parseFloat(toyDescription[4]), ageGroup, Boolean.parseBoolean(toyDescription[5]));
+            }
+        } catch(IllegalArgumentException e){
+            throw new ToyException("Wrong toy type");
         }
 
     }
